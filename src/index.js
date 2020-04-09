8e8060828119eebdf03a80c88881ee5fd66913b5
tg-void <tomasgia@tamu.edu>
2020-04-08 21:03:18 -0500

    very little

--- src/index.js
+++ src/index.js
@@ -46,9 +46,6 @@ class Application extends React.Component {
 ReactDOM.render(<Application />, document.getElementById('app'));
 
 
-
-
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
